8a16e41dd5dbb1d2bca7d22a67b0dc048c6ebabc
Michał Isalski <isalski.michal@gmail.com>
2023-08-06 22:24:05 +0200

    Bootstrap

--- src/cards/ServicesCard.tsx
+++ src/cards/ServicesCard.tsx
@@ -5,18 +5,21 @@ export function ServicesCard() {
 
     useEffect(() => {
         let clipNumbers: [number, number][] = [
-            [200, 300],
-            [250, 350],
-            [300, 300],
-            [350, 250],
-            [300, 200],
-        ]
-        const rotationNumbers = [0.99619469809, 0.08715574274] as const;
+            [  0*3,  50],
+            [-50*3,  75*3],
+            [  0*3, 100*3],
+            [  0*3, 200*3],
+            [ 30*3, 250*3],
+            [100*3, 200*3],
+            [100*3, 150*3],
+            [130*3,  75*3],
+        ];
+        const rotationNumbers = [0.99999048072, 0.00436330928] as const;
 
-        const clipNumbersOrigins = clipNumbers.map(c => [...c] as [number, number]);
+        const clipNumbersOrigins = clipNumbers.map(c => [c[0] - 10, c[1] - 10] as [number, number]);
         const vector = document.querySelector<HTMLDivElement>('#services .fancy-vector')!;
         const setter = (clips: [number, number][]) => {
-            const path = "M 200 150 Q 150 175, 200 200" + clips.map(c => ` T ${c[0]} ${c[1]}`).join('') + " T 200 150";
+            const path = `M ${clipNumbers[0][0]} ${clipNumbers[0][1]} Q ${clipNumbers[1][0]} ${clipNumbers[1][1]}, ${clipNumbers[2][0]} ${clipNumbers[2][1]}` + clips.slice(3).map(c => ` T ${c[0]} ${c[1]}`).join('') + ` T ${clipNumbers[0][0]} ${clipNumbers[0][1]}`;
             vector.style.clipPath = 'path(\''+path+'\')';
         }
         
@@ -24,12 +27,14 @@ export function ServicesCard() {
         const animator = () => {
             clipNumbers = clipNumbers.map((c, i) => {
                 const origin = clipNumbersOrigins[i];
-                const delta = [c[0] - origin[0], c[1] - origin[1]] as const;
-                const deltaRot = [
-                    delta[0] * rotationNumbers[0] - delta[1] * rotationNumbers[1],
-                    delta[0] * rotationNumbers[1] + delta[1] * rotationNumbers[0]
-                ] as const;
-                return [deltaRot[0] + origin[0], deltaRot[1] + origin[1]] as [number, number];
+                let delta = [c[0] - origin[0], c[1] - origin[1]] as const;
+                for(let j=0;j<(i%3)+2;j++) {
+                    delta = [
+                        delta[0] * rotationNumbers[0] - delta[1] * rotationNumbers[1],
+                        delta[0] * rotationNumbers[1] + delta[1] * rotationNumbers[0]
+                    ];
+                }
+                return [delta[0] + origin[0], delta[1] + origin[1]] as [number, number];
             })
             setter(clipNumbers);
             requestAnimationFrame(animator);
@@ -40,6 +45,7 @@ export function ServicesCard() {
     return (
       <div id="services" className='card'>
         <div className="fancy-vector"></div>
+        
       </div>
     )
 }
