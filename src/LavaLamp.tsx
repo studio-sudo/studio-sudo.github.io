8054654978f507b5f00f8c5bb945b51334b819ce
Michał Isalski <isalski.michal@gmail.com>
2023-08-23 01:21:15 +0200

    Fixed the lava lamp

--- src/LavaLamp.tsx
+++ src/LavaLamp.tsx
@@ -2,12 +2,11 @@ import { IsoLine, IsoPoint, isoLines } from 'marchingsquares';
 import { useEffect } from 'react';
 
 function pointToCommand(p: IsoPoint, index: number): string {
-    const size = document.body.clientWidth/GRID_SIZE;
-    const coords = `${p[0]*size} ${p[1]*size}` as const;
+    const size = document.body.clientWidth/GRID_SIZE + 1;
     if (index === 0) {
-        return `M ${coords}`;
+        return `M ${p[0]*size} ${p[1]*size}`;
     } else {
-        return `L ${coords}`;
+        return `L ${p[0]*size} ${p[1]*size}`;
     }
 }
 
@@ -17,14 +16,24 @@ function smooth(line: IsoLine): IsoLine {
 }
 
 function refine(line: IsoLine): IsoLine {
-    return line.flatMap((p1, i, points) => {
-        const p2 = i === points.length - 1 ? points[0] : points[i + 1];
-        return [p1, [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2] as IsoPoint] as const;
+    const result: IsoLine = [];
+
+    line.forEach((p1, i) => {
+        const p2 = i === line.length - 1 ? line[0] : line[i + 1];
+        result.push(p1);
+        result.push([(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2] as IsoPoint);
     });
+    return result;
 }
 
 function dual(line: IsoLine): IsoLine {
-    return refine(line).filter((_, i) => (i%2) === 1);
+    const result: IsoLine = [];
+
+    line.forEach((p1, i) => {
+        const p2 = i === line.length - 1 ? line[0] : line[i + 1];
+        result.push([(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2] as IsoPoint);
+    });
+    return result;
 }
 
 const GRID_SIZE = 40;
@@ -35,9 +44,9 @@ export function LavaLamp() {
 
         const line = [...Array(GRID_SIZE).keys()].map(() => 0);
 
-        let data: number[][] = [];
+        const data: number[][] = [];
 
-        for (let i = 0; i <= document.body.clientHeight * GRID_SIZE / document.body.clientWidth; i++) {
+        for (let i = 0; i <= Math.ceil(document.body.clientHeight * GRID_SIZE / document.body.clientWidth) + 2; i++) {
             data.push([...line]);
         }
 
@@ -46,24 +55,17 @@ export function LavaLamp() {
             { x: 12, y: 0, r: 20, dy: 0 },
             { x: 22, y: 0, r: 20, dy: 0 },
             { x: 32, y: 0, r: 20, dy: 0 },
-            { x: 42, y: 0, r: 20, dy: 0 },
-
-            { x: 30, y: 12, r: 20, dy: 2 },
-            { x: 4, y: 2, r: 2, dy: 1 },
-            { x: 7, y: 7, r: 3, dy: 1.2 },
-            
-            { x: 7, y: 22, r: 13, dy: 2 },
-            { x: 4, y: 12, r: 2, dy: 1 },
-            { x: 30, y: 17, r: 3, dy: 1.2 },
-
-            { x: 30, y: 32, r: 20, dy: 2 },
-            { x: 4, y: 22, r: 2, dy: 1 },
-            { x: 7, y: 27, r: 3, dy: 1.2 },
-
-            { x: 7, y: 42, r: 10, dy: 2 },
-            { x: 4, y: 32, r: 2, dy: 1 },
-            { x: 30, y: 37, r: 3, dy: 1.2 }
+            { x: 42, y: 0, r: 20, dy: 0 }
         ];
+
+        for (let i = 0; i < 20; i++) {
+            blobs.push({
+                x: Math.trunc(Math.random() * data[0].length),
+                y: Math.trunc(Math.random() * data.length),
+                r: 3 + Math.trunc(Math.random() * 17),
+                dy: 0.5 + Math.random() * 1.5,
+            })
+        }
 
         const vector = document.querySelector<HTMLDivElement>('#lava-lamp')!;
 
@@ -78,16 +80,27 @@ export function LavaLamp() {
         }
         
         setter(data);
-        const animator = () => {
-            data = data.map(line => line.map(() => 1));
-            
+
+        let lastTimestamp: DOMHighResTimeStamp = 0;
+        const animator = (timestamp: DOMHighResTimeStamp) => {
+            const delta = (timestamp - lastTimestamp) / 1000;
+            lastTimestamp = timestamp;
+            if (delta <= 0) return;
+
             blobs.forEach(blob => {
-                blob.y += blob.dy / 60;
-                if (blob.y + blob.r * 2 > data.length) {
+                blob.y += blob.dy * delta;
+                if (blob.y - blob.r > data.length) {
                     blob.y = -blob.r;
                 }
-                data = data.map((line, y) => line.map((a, x) => a - blob.r / ((blob.x-x) * (blob.x-x) + (blob.y-y) * (blob.y-y))));
-            })
+            });
+
+            data.forEach((line, y) => line.forEach((_, x) => {
+                let result = 1;
+                blobs.forEach(blob => {
+                    result -= blob.r / ((blob.x-x) * (blob.x-x) + (blob.y-y) * (blob.y-y));
+                });
+                data[y][x] = result;
+            }));
 
             setter(data);
             requestAnimationFrame(animator);
