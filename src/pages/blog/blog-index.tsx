25304e18afd73e9848d79c25c15009483130545a
Magdalena Jarząbek <magdalena.jarzabek@columbusglobal.com>
2023-08-31 22:41:07 +0200

    blog index

--- src/pages/blog/blog-index.tsx
+++ src/pages/blog/blog-index.tsx
@@ -6,11 +6,25 @@ const blogPosts = import.meta.compileTime<BlogPostsInfo>("./post-meta.ts")
 
 function BlogIndex() {
   return (
-    <div className='blogIndex'>
-      {blogPosts.map(post => (<Link key={post.slug} to={post.slug}>
-        <h2>{post.title}</h2>
-        <p>{post.description}</p>
-      </Link>))}
+    <div className='blogContainer'>
+      <div className='leftContainer'>
+        {blogPosts.map(post => (<div className='articleItem' key={post.slug}>
+          <h2 className='articleTitle'><a href={post.slug}>{post.title}</a></h2>
+          <p>{post.description}</p>
+          <a className='readMoreLink' href={post.slug}>Czytaj dalej »</a>
+        </div>))}
+      </div>
+      <div className='rightContainer'>
+        <div className='welcomeText'>
+          <h2>Welcome to Comunity</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div className='featuredPosts'>
+          {blogPosts.map(post => (<div className='articleItem' key={post.slug}>
+            <h2 className='articleTitle'><a href={post.slug}>{post.title}</a></h2>
+          </div>))}
+        </div>
+      </div>
     </div>
   )
 }
