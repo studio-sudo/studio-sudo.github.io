aed503438d89b427aa1e8aa92713487f514d32d3
Magdalena Jarząbek <magdalena.jarzabek@columbusglobal.com>
2023-09-02 23:22:19 +0200

    changes

--- src/pages/start/cards/BlogCard.tsx
+++ src/pages/start/cards/BlogCard.tsx
@@ -1,17 +1,11 @@
 import './BlogCard.scss';
 import { BlogPostsInfo } from '../../blog/post-meta';
 import { Link } from 'react-router-dom';
-import { hashCode } from '../../../utils';
+import { Button } from '../../../components/Button';
 
 const blogPosts = import.meta.compileTime<BlogPostsInfo>("../../blog/post-meta.ts")
 
 export function BlogCard() {
-
-    const getRandomColor = (data: string) => {
-        // const colors: string[] = ["#fa555c", "#ff9791", "#01cc9d", "#058a75", "#055844"];
-        // return colors[hashCode(data) % colors.length];
-        return "#f5e5e5";
-    }
 
     const posts = [...blogPosts]
         .sort((a, b) => a.createdOn.valueOf() - b.createdOn.valueOf())
@@ -27,13 +21,18 @@ export function BlogCard() {
             <div className='blog-index row g-0 px-5'>
                 {posts.map(post => (
                     <div className='col-lg-4 col-12'>
-                        <article className='blog-article-card of-color' style={{ backgroundColor: getRandomColor(post.title) }}>
+                        <article className='blog-article-card of-color'>
                             <div className='creator'>{post.createdBy}</div>
                             <div className='date'>{post.createdOn.toLocaleDateString()}</div>
                             <Link to={`/blog/${post.slug}`} className='title'><div>{post.title}</div></Link>
                         </article>
                     </div>
                 ))}
+            </div>
+            <div className='row g-0'>
+                <div className='col-12 px-4 pt-4'>
+                    <Button label="Czytaj więcej" />
+                </div>
             </div>
         </div>
     )
